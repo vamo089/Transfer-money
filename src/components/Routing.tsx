736d23d52a68982c41d1c43b9c400ad7b8b2c272
Yaroslav <yaroslav.burtelov@gmail.com>
2021-08-05 18:55:22 -0400

    routing refactor

--- src/components/Routing.tsx
+++ src/components/Routing.tsx
@@ -1,18 +1,23 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { ROUTES } from 'helpers/constants';
 import cookies from 'js-cookie';
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Route, useHistory, useLocation } from 'react-router-dom';
 import { RootState, useAppDispatch } from 'store';
 import { setToken } from 'store/reducers/auth';
 
 const Account = React.lazy(() => import('components/account'));
 const Auth = React.lazy(() => import('components/auth'));
 const Routing: React.FC = () => {
+  const location = useLocation();
+  const history = useHistory();
+  useEffect(() => {
+    history.push(token ? ROUTES.account : ROUTES.login);
+  }, [location]);
+
   const dispatch = useAppDispatch();
   const token = useSelector<RootState>((state) => state.auth.token) || cookies.get('token');
-
   const cookieToken = cookies.get('token');
 
   if (cookieToken && token !== cookieToken) {
@@ -20,9 +25,13 @@ const Routing: React.FC = () => {
   }
   return (
     <>
-      <Redirect from="*" to={token ? ROUTES.account : ROUTES.login} />
       <Suspense fallback={<CircularProgress />}>
-        {token ? <Route to={ROUTES.account} component={Account} /> : <Auth />}
+        <Route path={ROUTES.login}>
+          <Auth />
+        </Route>
+        <Route path={ROUTES.account}>
+          <Account />
+        </Route>
       </Suspense>
     </>
   );
