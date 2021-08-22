f03b1fb0b0a41c117a1591da31a569e7d7a3343b
Yaroslav <yaroslav.burtelov@gmail.com>
2021-08-22 12:37:19 -0400

    router refactor

--- src/components/Routing.tsx
+++ src/components/Routing.tsx
@@ -3,37 +3,29 @@ import { ROUTES } from 'helpers/constants';
 import cookies from 'js-cookie';
 import React, { Suspense, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, useHistory, useLocation } from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 import { RootState, useAppDispatch } from 'store';
 import { setToken } from 'store/reducers/auth';
 
 const Account = React.lazy(() => import('components/account'));
 const Auth = React.lazy(() => import('components/auth'));
-
 export const Routing: React.FC = () => {
-  const location = useLocation();
-  const history = useHistory();
-
-  useEffect(() => {
-    history.push(token ? ROUTES.account : ROUTES.login);
-  }, [location]);
-
   const dispatch = useAppDispatch();
+  const history = useHistory();
   const token = useSelector<RootState>((state) => state.auth.token) || cookies.get('token');
   const cookieToken = cookies.get('token');
-
   if (cookieToken && token !== cookieToken) {
     dispatch(setToken(cookieToken));
   }
+
+  useEffect(() => {
+    history.push(token ? ROUTES.account : ROUTES.login);
+  }, [token]);
+
   return (
     <>
       <Suspense fallback={<CircularProgress />}>
-        <Route path={ROUTES.login}>
-          <Auth />
-        </Route>
-        <Route path={ROUTES.account}>
-          <Account />
-        </Route>
+        {token ? <Route to={ROUTES.account} component={Account} /> : <Auth />}
       </Suspense>
     </>
   );
